c620f54558fadb97f5adc475798c1d722d3c3aa6
GITHUBXIONGXINOG <2017211677@stu.cqupt.edu.cn>
2021-03-09 16:09:48 +0800

    first commit

---
+++ model/db.js
@@ -0,0 +1,39 @@
+/*Schema、Model、Entity或者Documents的关系请牢记，
+Schema生成Model，Model创造Entity，
+Model和Entity都可对数据库操作造成影响
+，但Model比Entity更具操作性。*/
+ 
+const mongoose = require('mongoose');
+// 连接数据库 如果不自己创建 默认test数据库会自动生成
+mongoose.connect('mongodb://localhost/todo',{  useNewUrlParser: true,useUnifiedTopology: true});
+ 
+// 为这次连接绑定事件
+const db = mongoose.connection;
+db.once('error',() => console.log('Mongo connection error'));
+db.once('open',() => console.log('Mongo connection successed'));
+/************** 定义模式loginSchema **************/
+const loginSchema = mongoose.Schema({
+    account : String,
+    password : String
+});
+const taskSchema = mongoose.Schema({
+    task: String,
+    steps: Object,
+    today: Boolean,
+    pick: String,
+    files: Object,
+    note: String 
+})
+/************** 定义模型Model **************/
+/* const Models = {
+    Login : mongoose.model('Login',loginSchema),
+    Task : mongoose.model('Task',taskSchema)
+} */
+const User = mongoose.model('Login',loginSchema);
+const Task = mongoose.model('Task',taskSchema);
+
+ 
+module.exports = {
+    User,
+    Task,
+};
