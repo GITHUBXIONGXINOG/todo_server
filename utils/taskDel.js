1398b6f4eae2cf5bb0b4447e85dee70dc73134c4
GITHUBXIONGXINOG <2017211677@stu.cqupt.edu.cn>
2021-03-14 23:54:32 +0800

    删除修改:删除时删除全部该分类id的页面

--- utils/taskDel.js
+++ utils/taskDel.js
@@ -1,14 +1,18 @@
 //导入task分类构造函数
 const { ClassTitle } = require('../model/classtitle')
 const { Article } = require('../model/article')
-module.exports = async (req,res) =>{
-    const {type, _id} = JSON.parse(req.query.keyword)
-    if(type=='task'){//删除task
-         console.log('删除task');
-        await Article.deleteOne({_id})  
-    } else if(type=='class'){
+module.exports = async (req, res) => {
+    const { type, _id } = JSON.parse(req.query.keyword)
+    if (type == 'task') {//删除task
+        console.log('删除task');
+        await Article.deleteOne({ _id })
+    } else if (type == 'class') {
         console.log('删除class');
-        await ClassTitle.deleteOne({_id})  
+        //删除该分类内容
+        await Article.deleteMany({ classtitle: _id });
+        //删除分类
+        await ClassTitle.deleteOne({ _id });
+
     }
     res.send({ 'status': '1000', 'message': '删除数据成功' })
 
