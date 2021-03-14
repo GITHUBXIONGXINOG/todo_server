750a653b316f3b0ac8a485a40a6fee545849c798
GITHUBXIONGXINOG <2017211677@stu.cqupt.edu.cn>
2021-03-14 20:57:27 +0800

    删除task和class

--- utils/taskDel.js
+++ utils/taskDel.js
@@ -1,13 +1,14 @@
+//导入task分类构造函数
+const { ClassTitle } = require('../model/classtitle')
 const { Article } = require('../model/article')
 module.exports = async (req,res) =>{
-    console.log('删除task');
     const {type, _id} = JSON.parse(req.query.keyword)
     if(type=='task'){//删除task
+         console.log('删除task');
         await Article.deleteOne({_id})  
-        
-
     } else if(type=='class'){
-
+        console.log('删除class');
+        await ClassTitle.deleteOne({_id})  
     }
     res.send({ 'status': '1000', 'message': '删除数据成功' })
 
