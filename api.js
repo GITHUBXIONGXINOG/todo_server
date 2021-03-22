e27b96e40104288a268e599bab59b75960a656a1
GITHUBXIONGXINOG <2017211677@stu.cqupt.edu.cn>
2021-03-22 16:24:47 +0800

    修改api为todoapi

--- api.js
+++ api.js
@@ -10,11 +10,11 @@ const router = express.Router();
 /************** 创建(create) 读取(get) 更新(update) 删除(delete) **************/
 
 // 创建账号接口
-router.post('/api/user/register', require('./utils/regist'));
+router.post('/todoapi/user/register', require('./utils/regist'));
 // 登录账号接口
-router.post('/api/user/login', require('./utils/login'));
+router.post('/todoapi/user/login', require('./utils/login'));
 // 获取所有账号接口
-router.get('/api/user/all', (req, res) => {
+router.get('/todoapi/user/all', (req, res) => {
     // 通过模型去查找数据库
     User.find((err, data) => {
         if (err) {
@@ -25,7 +25,7 @@ router.get('/api/user/all', (req, res) => {
     });
 });
 //存储task
-router.post('/api/task', (req, res) => {
+router.post('/todoapi/task', (req, res) => {
     Task.create({ task: req.body.task }, (err, data) => {
         if (!err) {
             // res.send(err);
@@ -44,26 +44,26 @@ router.post('/api/task', (req, res) => {
     })
 })
 //退出账号
-router.get('/api/logout', require('./utils/logout'));
+router.get('/todoapi/logout', require('./utils/logout'));
 //添加分类
-router.post('/api/tasks/class/add',require('./utils/taskClassAdd'))
+router.post('/todoapi/tasks/class/add',require('./utils/taskClassAdd'))
 //获取分类
-router.post('/api/tasks/class/get',require('./utils/taskClassGet'))
+router.post('/todoapi/tasks/class/get',require('./utils/taskClassGet'))
 
 //存储文字task
-router.post('/api/task_add',require('./utils/taskAdd'))
+router.post('/todoapi/task_add',require('./utils/taskAdd'))
 //获取分类页内容
-router.post('/api/task_get',require('./utils/taskGet'))
+router.post('/todoapi/task_get',require('./utils/taskGet'))
 //查询当前的用户信息,id,登录状态 
-router.use('/api/loginstatus',require('./utils/loginStatus'))
+router.use('/todoapi/loginstatus',require('./utils/loginStatus'))
 //查询task
-router.get('/api/search',require('./utils/search')) 
+router.get('/todoapi/search',require('./utils/search')) 
 //更新task状态
-router.get('/api/task_update',require('./utils/taskUpdate'))
+router.get('/todoapi/task_update',require('./utils/taskUpdate'))
 //删除task
-router.get('/api/task_del',require('./utils/taskDel'))
+router.get('/todoapi/task_del',require('./utils/taskDel'))
 //当前的task
-router.get('/api/taskinfo_get',require('./utils/taskInfoGet'))
+router.get('/todoapi/taskinfo_get',require('./utils/taskInfoGet'))
 //上传头像昵称
- router.post('/api/info_updload',require('./utils/infoUpdload'))
+ router.post('/todoapi/info_updload',require('./utils/infoUpdload'))
 module.exports = router;
